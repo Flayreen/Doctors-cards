5c285602698c1ca528fe41c0137ec10e91fb27f8
whitehoof <whitehoof@gmail.com>
2023-11-14 18:07:36 +0200

    Реалізовано функціонал фільтрів БЕЗ запитів до сервера: працює локально, у DOM-і. Більше коментарів у файлі "index-30-mainblock.html".

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -3,5 +3,15 @@ import VisitCardiologist	from "./Classes/VisitCardiologist.js";
 import VisitDentist			from "./Classes/VisitDentist.js";
 import VisitTherapist		from "./Classes/VisitTherapist.js";
 
-import filterCards			from "./functions/filter-cards.js";
+import {filterCards, filtersReset} from "./functions/filter.js";
 
+
+// єдиний слухач для всіх фільтрів:
+document.querySelector('.filters__wrapper')
+	.addEventListener('input', () => {
+		filterCards('.mainblock .container');
+});
+
+// слухач для кнопки очищення фільтрів:
+document.querySelector('#filter-reset')
+	.addEventListener('click',filtersReset);
