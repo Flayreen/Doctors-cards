1b6040b90904f2452c221a3e2410b72d57799eb8
Olexandra Usyk <sandra09910work@gmail.com>
2023-11-19 13:38:03 +0100

    add get token,header

---
+++ src/scripts/API/getToken.js
@@ -0,0 +1,20 @@
+const ourLoginPas = {
+    email: "OSD@gmail.com", 
+    password: "12345"
+}
+// a64a6977-7d7a-4d2f-93cf-375935b363a8
+// user це обєкт який формується після введення данних в форму логінізаціі.
+//oбєкт  ourLoginPas зареєстрований на сайті,що вказано в тз
+
+async function getToken(user) {
+    try {
+        let {data} = await axios.post("https://ajax.test-danit.com/api/v2/cards/login", user )
+        console.log(data)
+        localStorage.setItem("token", data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default getToken;
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,22 +1,41 @@
 // API:
-import postVisit					from "./API/postVisit.js";
-import getVisits					from "./API/getVisits.js";
+//import postVisit from "./API/postVisit.js";
+import getVisits from "./API/getVisits.js";
+import getToken from "./API/getToken.js";
 
 //	CLASSES:
-import Visit						from "./Classes/Visit.js";
-import VisitCardiologist			from "./Classes/VisitCardiologist.js";
-import VisitDentist					from "./Classes/VisitDentist.js";
-import VisitTherapist				from "./Classes/VisitTherapist.js";
+import Visit from "./Classes/Visit.js";
+import VisitCardiologist from "./Classes/VisitCardiologist.js";
+import VisitDentist from "./Classes/VisitDentist.js";
+import VisitTherapist from "./Classes/VisitTherapist.js";
 
 //	FUNCTIONS:
-import {filterCards, filtersReset} from "./functions/filter.js";
+import { filterCards, filtersReset } from "./functions/filter.js";
 
 
 
 // єдиний слухач для всіх фільтрів:
 document.querySelector('.filters__wrapper').addEventListener('input', () => {
-		filterCards('.mainblock .container');
+	filterCards('.mainblock .container');
 });
 
 // слухач для кнопки очищення фільтрів:
-document.querySelector('#filter-reset').addEventListener('click',filtersReset);
+document.querySelector('#filter-reset').addEventListener('click', filtersReset);
+
+//робота з формою
+const formLogin = document.getElementById('form-login');
+formLogin.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const formData = new FormData(formLogin);
+	const passwordUser = formData.get('passwordUser');
+	const emailUser = formData.get('emailUser');
+	let user = { email: `${emailUser}`, password: `${passwordUser}` };
+	console.log(passwordUser);
+	console.log(emailUser);
+	console.log(user);
+	getToken(user);
+	formLogin.reset();
+});
+
+let TOKEN_FROM_LOCALSTORAGE = localStorage.getItem("token");
+console.log(TOKEN_FROM_LOCALSTORAGE);
